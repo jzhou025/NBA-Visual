9a6bdaeeb4b221b20c763d28cf06099b88617104
jzhou025 <jingyuzhou77@gmail.com>
2019-03-31 16:48:46 -0700

    update profile style

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import { ShotChart } from './ShotChart';
 import { Profile } from './Profile';
+import nba from 'nba';
 
 export class Main extends React.Component {
+    state = {
+        playerId: 2544,
+        playerInfo: {},
+    }
+    componentDidMount() {
+        nba.stats.playerInfo({ PlayerID: this.state.playerId })
+            .then((info) => {
+                const playerInfo = Object.assign({},
+                    info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
+                console.log("final player info", playerInfo);
+                this.setState({ playerInfo });
+            })
+            .catch((e) => {
+                console.log(e);
+            });
+    }
+
     render() {
         return (
             <div className="main">
-                <Profile />
-                <ShotChart playerId={201939} />
+                <Profile playerInfo={this.state.playerInfo} />
+                <ShotChart playerId={this.state.playerId} />
             </div>
         );
     }
