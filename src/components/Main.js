e42720fd6fdae628c2f2e8008d94ca7097f33608
jzhou025 <jingyuzhou77@gmail.com>
2019-04-07 14:45:09 -0700

    add a separate data container

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,32 +1,32 @@
 import React from 'react';
-import { ShotChart } from './ShotChart';
+import { DataViewContainer } from './DataViewContainer';
 import { Profile } from './Profile';
 import nba from 'nba';
 
 export class Main extends React.Component {
-    state = {
-        playerId: 2544,
-        playerInfo: {},
-    }
-    componentDidMount() {
-        nba.stats.playerInfo({ PlayerID: this.state.playerId })
-            .then((info) => {
-                const playerInfo = Object.assign({},
-                    info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
-                console.log("final player info", playerInfo);
-                this.setState({ playerInfo });
-            })
-            .catch((e) => {
-                console.log(e);
-            });
-    }
+  state = {
+    playerId: 201939,
+    playerInfo: {},
+  }
+  componentDidMount() {
+    nba.stats.playerInfo({ PlayerID: this.state.playerId })
+      .then((info) => {
+        const playerInfo = Object.assign({}, 
+          info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
+        console.log("final player info", playerInfo);
+        this.setState({ playerInfo });
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
 
-    render() {
-        return (
-            <div className="main">
-                <Profile playerInfo={this.state.playerInfo} />
-                <ShotChart playerId={this.state.playerId} />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="main">
+        <Profile playerInfo={this.state.playerInfo}/>
+        <DataViewContainer playerId={this.state.playerId}/>
+      </div>
+    );
+  }
 }
